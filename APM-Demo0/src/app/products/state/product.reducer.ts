5b14e464a916f211f12fc30436b432aaed1b85f3
i.sirotinina <sirotininailona@gmail.com>
2021-01-22 03:21:40 +0300

    1) create state/products reducer

---
+++ APM-Demo0/src/app/products/state/product.reducer.ts
@@ -0,0 +1,13 @@
+/* NgRx */
+import { createReducer, on, createAction } from '@ngrx/store';
+
+export const productReducer = createReducer(
+  { showProductCode: true },
+  on(createAction('[Product] Toggle Product Code'), state => {
+    return {
+      ...state,
+      showProductCode: !state.showProductCode
+    };
+  })
+);
+
