2d9a86ca6f3664eac50a6873d57e0052f7322464
i.sirotinina <sirotininailona@gmail.com>
2021-01-22 03:49:21 +0300

    8) add interface

--- APM-Demo0/src/app/products/state/product.reducer.ts
+++ APM-Demo0/src/app/products/state/product.reducer.ts
@@ -1,10 +1,21 @@
 /* NgRx */
 import { createReducer, on } from '@ngrx/store';
 import * as ProductActions from './product.actions';
+import * as AppState from '../../state/app.state';
+
+export interface State extends AppState.State {
+  products: ProductState;
+}
+
+// State for this feature (Product)
+export interface ProductState {
+  showProductCode: boolean;
+}
+
 
 export const productReducer = createReducer(
   {showProductCode: true},
-  on(ProductActions.toggleProductCode, state => {
+  on(ProductActions.toggleProductCode, (state): ProductState => {
     return {
       ...state,
       showProductCode: !state.showProductCode
